93aa8d31e0bab05c6d912109a799a24f5b52d11d
Aro <aro.jormand@gmail.com>
2024-04-22 22:45:45 +0200

    navbar

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 
-import { headingFont, textFont } from "@/public/fonts/fonts";
+import { headingFont } from "@/public/fonts/fonts";
+import logo from "@/public/images/logo.svg";
 
 export const Logo = () => {
   return (
     <Link href="/">
-      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:felx">
-        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src={logo} alt="Logo" height={30} width={30} />
 
         <p
           className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
