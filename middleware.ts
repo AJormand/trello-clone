be6a90f8210c315010fd97b08464cb80cb43cf12
Aro <aro.jormand@gmail.com>
2024-04-23 23:03:01 +0200

    org-control

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,10 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 //specify protected routes - no routes protected by default anymore
-const isProtectedRoute = createRouteMatcher(["/select-org(.*)", "/forum(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/select-org(.*)",
+  "/organization(.*)",
+]);
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
