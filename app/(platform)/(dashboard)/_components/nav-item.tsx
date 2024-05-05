f8afb71a653f0787e716b55d7b3e76049ab079dd
Aro <aro.jormand@gmail.com>
2024-05-05 21:01:18 +0200

    nav

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,8 +1,16 @@
 "use client";
 
-import { AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { useRouter, usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { Activity, CreditCard, Layout, Settings } from "lucide-react";
 
 export type Organization = {
   id: string;
@@ -23,6 +31,36 @@ export const NavItem = ({
   organization,
   onExpand,
 }: NavItemProps) => {
+  const router = useRouter();
+  const pathName = usePathname();
+
+  const routes = [
+    {
+      label: "Boards",
+      icon: <Layout className="w-4 h-4 mr-2" />,
+      href: `/organizations/${organization.id}`,
+    },
+    {
+      label: "Activity",
+      icon: <Activity className="w-4 h-4 mr-2" />,
+      href: `/organizations/${organization.id}/activity`,
+    },
+    {
+      label: "Settings",
+      icon: <Settings className="w-4 h-4 mr-2" />,
+      href: `/organizations/${organization.id}/settings`,
+    },
+    {
+      label: "Billing",
+      icon: <CreditCard className="w-4 h-4 mr-2" />,
+      href: `/organizations/${organization.id}/billing`,
+    },
+  ];
+
+  const onClick = (href: string) => {
+    router.push(href);
+  };
+
   return (
     <div>
       <AccordionItem value={organization.id} className="border-none">
@@ -30,7 +68,7 @@ export const NavItem = ({
           onClick={() => onExpand(organization.id)}
           className={cn(
             "flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline",
-            isActive && !isExpanded && "bg-sky-500-10 text-sky-700"
+            isActive && !isExpanded && "bg-sky-500/10 text-sky-700"
           )}
         >
           <div className="flex items-center gap-x-2">
@@ -42,8 +80,26 @@ export const NavItem = ({
                 className="rounded-sm object-conver"
               />
             </div>
+            <span className="font-medium text-sm">{organization.name}</span>
           </div>
         </AccordionTrigger>
+        <AccordionContent className="pt-1 text-neutral-700">
+          {routes.map((route) => (
+            <Button
+              key={route.href}
+              size="sm"
+              onClick={() => onClick(route.href)}
+              className={cn(
+                "w-full font-normal, justify-start pl-10 mb-1",
+                pathName === route.href && "bg-sky-500/10 text-sky-700"
+              )}
+              variant="ghost"
+            >
+              {route.icon}
+              {route.label}
+            </Button>
+          ))}
+        </AccordionContent>
       </AccordionItem>
     </div>
   );
