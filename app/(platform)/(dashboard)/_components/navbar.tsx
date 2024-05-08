1dd48948839137dfb0bb17052d2af8675633360a
Aro <aro.jormand@gmail.com>
2024-05-08 23:14:26 +0200

    mobile-navigation

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -2,12 +2,16 @@ import { Plus } from "lucide-react";
 
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
+
+import { MobileSidebar } from "./mobile-sidebar";
+
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 export const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      {/* todo: mobile sidebar */}
+      <MobileSidebar />
+
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
